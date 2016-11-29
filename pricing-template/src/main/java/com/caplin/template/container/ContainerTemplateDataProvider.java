43f02a6f19595a11e160d182615471e28791d785
Ross Anderson <rossdanderson@gmail.com>
2016-11-29 15:19:04 +0000

    Separate Container/Row/Type2 publishers Send container subscriptions via Transformer

--- pricing-template/src/main/java/com/caplin/template/container/ContainerTemplateDataProvider.java
+++ pricing-template/src/main/java/com/caplin/template/container/ContainerTemplateDataProvider.java
@@ -1,68 +1,57 @@
 package com.caplin.template.container;
 
-import java.util.logging.Level;
-
 import com.caplin.datasource.DataSource;
 import com.caplin.datasource.messaging.container.ContainerMessage;
-import com.caplin.datasource.messaging.record.RecordType1Message;
-import com.caplin.datasource.messaging.record.RecordType2Message;
+import com.caplin.datasource.messaging.record.GenericMessage;
 import com.caplin.datasource.namespace.PrefixNamespace;
 import com.caplin.datasource.publisher.ActivePublisher;
 import com.caplin.datasource.publisher.DataProvider;
 import com.caplin.datasource.publisher.DiscardEvent;
 import com.caplin.datasource.publisher.RequestEvent;
 
-public class ContainerTemplateDataProvider implements DataProvider {
+public class ContainerTemplateDataProvider {
 
     private static final int NUM_CONSTITUENTS = 50;
     private final DataSource dataSource;
 
-    private ActivePublisher publisher;
+    private ActivePublisher containerPublisher;
+    private ActivePublisher rowPublisher;
 
     public ContainerTemplateDataProvider(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     public void initialise() {
-        publisher = dataSource.createActivePublisher(new PrefixNamespace("/TEMPLATE/CONTAINER"), this);
-    }
-
-    @Override
-    public void onRequest(RequestEvent requestEvent) {
-        if (requestEvent.getSubject().equals("/TEMPLATE/CONTAINER/ALL")) {
-            ContainerMessage containerMessage = publisher.getMessageFactory().createContainerMessage(requestEvent.getSubject());
-            containerMessage.setDoNotAuthenticate(true);
+        containerPublisher = dataSource.createActivePublisher(new PrefixNamespace("/TEMPLATE/CONTAINER/ALL"), new DataProvider() {
+            @Override
+            public void onRequest(RequestEvent requestEvent) {
+                ContainerMessage containerMessage = containerPublisher.getMessageFactory().createContainerMessage(requestEvent.getSubject());
+                containerMessage.setDoNotAuthenticate(true);
 
 
-            for (int i = 0; i < NUM_CONSTITUENTS; i++) {
-                containerMessage.addElement("/TEMPLATE/CONTAINERROW_" + i);
-            }
-
-            containerMessage.setImage(true);
-            publisher.publishInitialMessage(containerMessage);
-        } else if (requestEvent.getSubject().startsWith("/TEMPLATE/CONTAINERROW")) {
-            RecordType1Message message = publisher.getMessageFactory().createRecordType1Message(requestEvent.getSubject());
-
-            message.setImage(true);
-            publisher.publishInitialMessage(message);
-        } else if (requestEvent.getSubject().equals("/TEMPLATE/TYPE2/ALL")) {
-            for (int i = 0; i < NUM_CONSTITUENTS; i++) {
-
-                RecordType2Message type2Message = publisher.getMessageFactory().createRecordType2Message(requestEvent.getSubject(), "TYPE2_KEY", String.valueOf(i));
-
-                if (i == 0) {
-                    type2Message.setImage(true);
-                    publisher.publishInitialMessage(type2Message);
-                } else {
-                    publisher.publishToSubscribedPeers(type2Message);
+                for (int i = 0; i < NUM_CONSTITUENTS; i++) {
+                    containerMessage.addElement("/TEMPLATE/CONTAINERROW_" + i);
                 }
+
+                containerMessage.setImage(true);
+                containerPublisher.publishInitialMessage(containerMessage);
             }
-        } else {
-            dataSource.getLogger().log(Level.INFO, "Unknown subject " + requestEvent.getSubject());
-        }
 
+            @Override
+            public void onDiscard(DiscardEvent discardEvent) {}
+        });
+
+        rowPublisher = dataSource.createActivePublisher(new PrefixNamespace("/TEMPLATE/CONTAINER/CONTAINERROW"), new DataProvider() {
+            @Override
+            public void onRequest(RequestEvent requestEvent) {
+                GenericMessage updateMessage = rowPublisher.getMessageFactory().createGenericMessage(requestEvent.getSubject());
+                updateMessage.setField("currentTime", String.valueOf(System.currentTimeMillis()));
+                rowPublisher.publishInitialMessage(updateMessage); // Publish this update to any new peers
+                rowPublisher.publishToSubscribedPeers(updateMessage); // Publish this update to any existing peers
+            }
+
+            @Override
+            public void onDiscard(DiscardEvent discardEvent) {}
+        });
     }
-
-    @Override
-    public void onDiscard(DiscardEvent discardEvent) {}
 }
