ad0c5bb78b436ca2f2ec7f2fb5f1689aab9ca261
Christoph Sitter <christoph.sitter@caplin.com>
2017-06-12 16:25:04 +0100

    Incorporate improvements from @rossanderson

--- pricing-template/src/main/java/com/caplin/template/PricingAdapterTemplate.java
+++ pricing-template/src/main/java/com/caplin/template/PricingAdapterTemplate.java
@@ -5,6 +5,7 @@ import com.caplin.datasource.DataSourceFactory;
 import com.caplin.template.channel.ChannelTemplate;
 import com.caplin.template.container.ContainerTemplateDataProvider;
 import com.caplin.template.pricing.PricingTemplateDataProvider;
+import com.caplin.template.type2.Type2DataProvider;
 
 public class PricingAdapterTemplate {
 
@@ -18,10 +19,12 @@ public class PricingAdapterTemplate {
         PricingTemplateDataProvider pricingTemplateDataProvider = new PricingTemplateDataProvider(dataSource);
         ContainerTemplateDataProvider containerTemplateDataProvider = new ContainerTemplateDataProvider(dataSource);
         ChannelTemplate channelTemplate = new ChannelTemplate(dataSource);
+        Type2DataProvider type2DataProvider = new Type2DataProvider(dataSource);
 
         pricingTemplateDataProvider.initialise();
         containerTemplateDataProvider.initialise();
         channelTemplate.initialise();
+        type2DataProvider.initialise();
     }
 
     public static void main(String[] args) {
