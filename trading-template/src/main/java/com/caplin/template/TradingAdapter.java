424730e32692310a063cdbf2de5abb3efe5f54d4
Jana Schoeller <Jana.Schoeller@caplin.com>
2017-06-26 15:04:22 +0100

    Fix TradingAdapter and PermissioningAdapter for training

--- trading-template/src/main/java/com/caplin/template/TradingAdapter.java
+++ trading-template/src/main/java/com/caplin/template/TradingAdapter.java
@@ -1,10 +1,14 @@
 package com.caplin.template;
 
+import java.io.IOException;
+import java.util.logging.Level;
+
+import com.caplin.datasource.ConnectionListener;
 import com.caplin.datasource.DataSource;
 import com.caplin.datasource.DataSourceFactory;
-import com.caplin.trading.*;
-
-import java.io.IOException;
+import com.caplin.datasource.PeerStatusEvent;
+import com.caplin.trading.TradingApplicationListener;
+import com.caplin.trading.TradingProvider;
 
 /**
  * This is the main class for the trading adapter template.
@@ -12,12 +16,15 @@ import java.io.IOException;
  * *** IMPORTANT ***
  * If this class is renamed, then the manifest section in the build file must be updated to reflect the name change
  */
-public class TradingAdapterTemplate {
+public class TradingAdapter implements ConnectionListener{
+
+    private final DataSource dataSource;
 
     private final TradingApplicationListener templateTradingApplicationListener;
     private final TemplateTradeListener templateTradeListener;
 
-    public TradingAdapterTemplate(DataSource dataSource) throws IOException {
+    public TradingAdapter(DataSource dataSource) throws IOException {
+        this.dataSource = dataSource;
 
         templateTradeListener = new TemplateTradeListener(dataSource.getLogger());
         templateTradingApplicationListener = new TemplateTradingApplicationListener(templateTradeListener, dataSource.getLogger());
@@ -25,10 +32,16 @@ public class TradingAdapterTemplate {
         new TradingProvider(templateTradingApplicationListener, dataSource);
     }
 
+    @Override
+    public void onPeerStatus(PeerStatusEvent peerStatusEvent) {
+        dataSource.getLogger().log(Level.INFO, "Peer status received: peer={0}, status={1}", new Object[] {peerStatusEvent.getPeer(), peerStatusEvent.getPeerStatus()});
+
+    }
+
     public static void main(String[] args) throws IOException {
         DataSource dataSource = DataSourceFactory.createDataSource(args);
 
-        new TradingAdapterTemplate(dataSource);
+        new TradingAdapter(dataSource);
 
         dataSource.start();
     }
