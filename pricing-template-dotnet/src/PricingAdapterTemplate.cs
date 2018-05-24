adacb52ef2b6fbc823db8fcc7fde05c11fa8948a
Andrew Sciascia <andy.sciascia@caplin.com>
2018-05-24 12:23:54 +0100

    PCINTS-325: Fixes fir .NET template.
    Set the Prefer32Bit property to false in the project file.
    Corrected the locations of the source files in the project file ( use '\' not '/' ).
    Add a startup script to the blade.
    Set the default config file to TemplatePricingAdapter.conf in code.
    Rename the blade config file to TemplatePricingAdapter.conf.
    
    Note that when I built this on Windows the macro reading worked.
    When we build the kit it doesn't copy all the libraries to bin/Release just the .NET one.

--- pricing-template-dotnet/src/PricingAdapterTemplate.cs
+++ pricing-template-dotnet/src/PricingAdapterTemplate.cs
@@ -33,7 +33,7 @@ namespace Caplin.Template
         {
 
             ILogger logger = new ConsoleLogger();
-            IDataSource dataSource = new Caplin.DataSource.DataSource("demosource.conf", args, logger);
+            IDataSource dataSource = new Caplin.DataSource.DataSource("TemplatePricingAdapter.conf", args, logger);
             var templateAdapter = new PricingAdapterTemplate(dataSource);
 
             templateAdapter.Initialise();
