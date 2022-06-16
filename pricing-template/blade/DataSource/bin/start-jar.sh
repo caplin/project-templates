de6a1ceb142987d34164617b624de79d3e3d215b
Daniel Hale <daniel.hale@caplin.com>
2022-06-16 10:13:09 +0100

    Revert "delete Blade/Datasource/bin and get start-jar.sh and overrides from gradle."
    This reverts commit 1471e71824d52fff37b36b32e3acb3a1687cee81.

---
+++ pricing-template/blade/DataSource/bin/start-jar.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Start the Pricing Template Java Adapter.
+#
+# $1 - Path to java executable
+# $2 - Path to datasource XML file
+# $3 - Path to fields XML file
+# $4 - Java definitions ( optional )
+#
+# Returns the process id of the Java process.
+#
+
+BLADENAME=@adapterName@
+
+if [ "$1" = "CONFREADER" ]; then
+   shift
+   confreading=1
+   jar=`ls "$BINARY_ROOT"/lib/datasource*.jar|head -1`
+else
+   confreading=0
+   jar=`ls "$BINARY_ROOT"/lib/$BLADENAME*.jar|head -1`
+   classpath="${BINARY_ROOT}/lib/*"
+
+   echo "Classpath: $jar"
+fi
+
+if [ $confreading = 1 ]; then
+   java -jar "$jar" "$@"
+   exit $?
+else
+   java -cp "$classpath" -jar $CAPLIN_BLADE_JAVA_OPTIONS "$jar" "$@" 2> "$LOGDIR"/java-$BLADENAME.log >/dev/null &
+   echo $!
+fi
