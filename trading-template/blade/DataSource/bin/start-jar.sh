311fff6e23756f1095b21e6cc56f76a11d6fce0f
Ian Selwyn-Smith <ian.selwyn-smith@caplin.com>
2024-02-14 08:33:55 +0000

    PCINTS-341 refactor start-jar.sh scripts

--- trading-template/blade/DataSource/bin/start-jar.sh
+++ trading-template/blade/DataSource/bin/start-jar.sh
@@ -14,20 +14,11 @@ BLADENAME=@adapterName@
 
 if [ "$1" = "CONFREADER" ]; then
    shift
-   confreading=1
-   jar=`ls "$BINARY_ROOT"/lib/datasource*.jar|head -1`
-else
-   confreading=0
-   jar=`ls "$BINARY_ROOT"/lib/$BLADENAME*.jar|head -1`
-   classpath="${BINARY_ROOT}/lib/*"
-
-   echo "Classpath: $jar"
-fi
-
-if [ $confreading = 1 ]; then
-   java -jar "$jar" "$@"
+   java -cp "${BINARY_ROOT}/lib/*" com.caplin.datasource.DataSource "$@"
    exit $?
 else
-   java -cp "$classpath" -jar $CAPLIN_BLADE_JAVA_OPTIONS "$jar" --trading-property-file=etc/trading-provider.properties "$@" 2> "$LOGDIR"/java-$BLADENAME.log >/dev/null &
+   jar=$(ls "${BINARY_ROOT}"/lib/${BLADENAME}*.jar|head -1)
+   echo "Jar: ${jar}"
+   java $CAPLIN_BLADE_JAVA_OPTIONS -jar "$jar" "$@" 2> "$LOGDIR"/java-$BLADENAME.log >/dev/null &
    echo $!
 fi
